be308fe416c6774bb42ded9ecb5e00d6ad74d699
hblancolo <hector.classicsound@gmail.com>
2017-03-16 11:57:10 +0100

    Add files via upload

---
+++ calcmatrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from matrix import MatrixOperations
+
+matrixCalculator = MatrixOperations()
+
+matrix_1 = 1
+matrix_2 = 2
+matrix_3 = matrixCalculator.add(matrix_1, matrix_2)
+print(matrix_3)
---
+++ matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+
+class MatrixOperations():
+
+    def add(self, m1, m2):
+        return m1 + m2
+
+    def mult(self, m1, m2):
+        return m1 * m2
+        
