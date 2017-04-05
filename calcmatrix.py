a002a64437e6c953273deb9f0c929e9c79e13e34
hblancolo <hector.classicsound@gmail.com>
2017-04-05 03:14:16 +0200

    matrix.py is the module, calcmatrix.py calls the module

--- calcmatrix.py
+++ calcmatrix.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
+# Code by Héctor Blanco Lozano
+
 from matrix import Matrix
 
-matrix_1 = Matrix(4,5,6,7)
-matrix_2 = Matrix(1,1,1,1)
-
+matrix_1 = Matrix([[1, 1], [1, 1]])
+matrix_2 = Matrix([[4, 1], [3, 1]])
 print(matrix_2)
 
 matrix_3 = matrix_1 + matrix_2
 print(matrix_3)
 
+matrix_4 = matrix_1 + 5
+print(matrix_4)
+
+matrix_5 = matrix_1 - matrix_2
+print(matrix_3)
+
+matrix_6 = matrix_1 - 5
+print(matrix_4)
+
+matrix_6 = matrix_1.prod(matrix_2)
+print(matrix_6)
+
+for v in matrix_2:
+    print(v)
